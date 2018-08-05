e55b769a82bd023505d2691bab73e0eb9b4c1302
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2018-08-06 02:08:43 +0700

    Add ListCalonActivity (still empty)

---
+++ app/src/main/java/com/preklit/taarufchat/activity/ListCalonActivity.kt
@@ -0,0 +1,12 @@
+package com.preklit.taarufchat.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListCalonActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_calon)
+    }
+}
